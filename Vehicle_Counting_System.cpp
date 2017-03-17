e021396266b65a34a6b6d6cdb84db9d9b8d93f51
yuyuyu <coywboyw@126.com>
2017-03-17 17:01:11 +0800

    对目标轮廓选定设置合适范围

--- Vehicle_Counting_System.cpp
+++ Vehicle_Counting_System.cpp
@@ -17,13 +17,6 @@ Mat MoveDetect(Mat frame1, Mat frame2)
 	adaptiveThreshold(diff, diff, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 15, 3);
 	//imshow("threshold", diff);
  
-	/*Mat element = getStructuringElement(MORPH_RECT, Size(3, 3));
-	Mat element2 = getStructuringElement(MORPH_RECT, Size(21, 21));
-	erode(diff, diff, element);
-	imshow("erode", diff);
-
-	dilate(diff, diff, element2);
-	imshow("dilate", diff);*/
 
 	Mat element = getStructuringElement(MORPH_RECT, Size(3, 3));
 	Mat element2 = getStructuringElement(MORPH_RECT, Size(21, 21));
@@ -35,18 +28,25 @@ Mat MoveDetect(Mat frame1, Mat frame2)
 	vector<vector<Point>> contours;  
 	vector<Vec4i> hierarcy;
 	findContours(diff, contours, hierarcy, CV_RETR_EXTERNAL, CHAIN_APPROX_NONE); //查找轮廓
+	vector<vector<Point>> contours_poly(contours.size());
 	vector<Rect> boundRect(contours.size()); //定义外接矩形集合
+
 	//drawContours(img2, contours, -1, Scalar(0, 0, 255), 1, 8);  //绘制轮廓
-	int x0=0, y0=0, w0=0, h0=0;
+	int x0=0, y0=0, w0=0, h0=0, CarNum = 0;
 	for(int i=0; i<contours.size(); i++)
 	{
-		boundRect[i] = boundingRect((Mat)contours[i]); //查找每个轮廓的外接矩形
-	
-		x0 = boundRect[i].x;  //获得第i个外接矩形的左上角的x坐标
-		y0 = boundRect[i].y; //获得第i个外接矩形的左上角的y坐标
-		w0 = boundRect[i].width; //获得第i个外接矩形的宽度
-		h0 = boundRect[i].height; //获得第i个外接矩形的高度
-		rectangle(result, Point(x0, y0), Point(x0+w0, y0+h0), Scalar(0, 255, 0), 2, 8); //绘制第i个外接矩形
+		approxPolyDP(Mat(contours[i]), contours_poly[i], 3, true); //多边形逼近
+		boundRect[i] = boundingRect((Mat)contours_poly[i]); //查找每个轮廓的外接矩形
+		if(boundRect[i].width > 55 && boundRect[i].width <180 && boundRect[i].height > 55 && boundRect[i].height < 180)
+		{
+			x0 = boundRect[i].x;  //获得第i个外接矩形的左上角的x坐标
+			y0 = boundRect[i].y; //获得第i个外接矩形的左上角的y坐标
+			w0 = boundRect[i].width; //获得第i个外接矩形的宽度
+			h0 = boundRect[i].height; //获得第i个外接矩形的高度
+			rectangle(result, Point(x0, y0), Point(x0+w0, y0+h0), Scalar(0, 255, 0), 2, 8); //绘制第i个外接矩形
+			
+		}
+
 	}
 	return result;	
 }
